aa36c36a6ef96cc6f0fbcf717a0880cc5ab83b4a
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-10 17:43:30 -0400

    squares

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+    results = []
+  array.each do |x|
+      results.push x*x
+end
+   results
+    end
+
+def new_square_array(array)
+      array.collect{|x| x*x}
 end
